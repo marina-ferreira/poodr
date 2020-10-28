2a17d495e307440ec88a5e78a40eaaf37454ef0d
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-10-28 19:05:25 -0300

    Chapter 6 | Misapplying Inheritance

---
+++ chapter_6/02_misapplying_inheritance.rb
@@ -0,0 +1,50 @@
+# bad ❌
+class Bicycle
+	attr_reader :style, :size, :tape_color
+
+	def initialize(args)
+		@style = args[:style]
+		@size = args[:size]
+		@tape_color = args[:tape_color]
+	end
+
+	def spares
+    {
+      chain: '10-speed',
+      tire_size: '23',
+      tape_color: tape_color
+    }
+	end
+end
+
+class MountainBike < Bicycle
+	attr_reader :front_shock, :rear_shock
+
+	def initialize(args)
+		@front_shock = args[:front_shock]
+		@rear_shock = args[:rear_shock]
+		super(args)
+	end
+
+	def spares
+		super.merge({ rear_shock: rear_shock })
+	end
+end
+
+mountain_bike = MountainBike.new(
+	size: 'S',
+	front_shock: 'Manitou',
+	rear_shock: 'Fox'
+)
+
+mountain_bike.size
+#=> 'S'
+
+mountain_bike.spares
+#=> {
+#  :tire_size => '23', (wrong)
+#  :chain => '10-speed',
+#  :tape_color => nil, (not applicable)
+#  :front_shock => 'Manitou',
+#  :rear_shock => 'Fox'
+# }
